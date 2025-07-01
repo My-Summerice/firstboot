d1bc25a0e030cdb33788fa81b5c9073235591afb
summerice <summerice325@gmail.com>
2025-07-01 16:54:43 +0800

    随便提交点东西

--- src/test/java/org/example/firstboot/FirstbootApplicationTests.java
+++ src/test/java/org/example/firstboot/FirstbootApplicationTests.java
@@ -1,11 +1,17 @@
 package org.example.firstboot;
 
+import javafx.application.Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.util.StopWatch;
 
-@SpringBootTest
+import static java.lang.Thread.sleep;
+
+@PropertySource(value = "classpath:application.properties")
+@SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class FirstbootApplicationTests {
 
     @Autowired
@@ -16,6 +22,18 @@ class FirstbootApplicationTests {
 
         redisTemplate.opsForValue().set("name", "张三");
 
+    }
+
+    @Test
+    void testStopWatch() throws InterruptedException {
+
+        StopWatch sw = new StopWatch();
+        sw.start("开始打印");
+        sleep(1000);
+        System.out.println(redisTemplate.opsForValue().get("name"));
+        sleep(1000);
+        sw.stop();
+        System.out.println(sw.shortSummary());
     }
 
 }
